fd1fca9992da55c8506d149bb85c26e7e24d55ed
Oksana Shyshkina <38759037-shyshkinaksysha@users.noreply.replit.com>
2025-01-07 19:39:21 +0000

    Lesson 2: Variables and data types

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+// Lesson 2
+// Variables and types of data
+
 fn main() {
-    println!("Hello, world!");
+  // Immutable variable
+  let x = 5;
+  println!("The value of x is: {}", x);
+
+  // Mutable variable
+  let mut y = 10;
+  println!("The value of y is: {}", y);
+
+  // Changing the value of y
+  y = 15;
+  println!("The new value of y is: {}", y);
 }
