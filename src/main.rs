08cf8afd98b9fc48a0552f8aee89892d2366bd80
Oksana Shyshkina <38759037-shyshkinaksysha@users.noreply.replit.com>
2025-01-08 19:58:04 +0000

    half lesson

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
-    println!("Hello, world!");
+    greet();
+    greet_user("Alice");
+  let sum = add(5, 3);
+  println!("The sum is: {}", sum);
+  let sum1 = add(10, 20);
+  println!("{}", sum1);
 }
+// example easy function
+fn greet() {
+    println!("Hello, Rust!");
+}
+
+//example function with argument
+fn greet_user(name: &str) {
+    println!("Hello {}", name);
+} 
+
+//example function with returning value
+fn add(x: i32, y: i32) -> i32 {
+  x + y
+} 
