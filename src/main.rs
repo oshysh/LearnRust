65bc8bb2727d4a7f4cbade03716ba72b236c3d5f
Oksana Shyshkina <38759037-shyshkinaksysha@users.noreply.replit.com>
2025-01-08 17:15:22 +0000

    Final version of lesson

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,69 @@
 fn main() {
-    println!("Hello, world!");
+
+  // Loop and break him
+  let mut counter = 0;
+  loop {
+      println!("Counter: {}", counter);
+      counter += 1;
+
+      if counter == 5 {
+          break; 
+      }
+  }
+
+  //Cycle While
+  let mut number = 3;
+  while number > 0{
+    println!("{}", number);
+    number -= 1;
+  }
+  println!("LiftOff");
+
+  //Cycle for
+  for number in 1..5 {
+    println!("Number: {}", number);
+  }
+
+  //work with Break and Continue for cycles
+  for number in 1..10 {
+    if number % 2 == 0{
+      continue;
+    }
+    if number > 7{
+      break;
+    }
+    println!("Odd number: {}", number);
+  }
+  // Loop cycle 10 to 1
+  let mut num = 10;
+  loop {
+    if num >=1 {
+      println!("Number: {}", num);
+      num -= 1;
+    } else {
+    println!("Liftoff!");
+    break;  
+    }
+    }
+  
+  // calculate all numbers from 1 to 10
+  let mut x = 1;
+  let mut y = 0;
+  while x<=10 {
+    println!("Addition: {}", x + y);
+    y = x + y;
+    x += 1;
+  }
+
+  //print all numbers 1-10 without %3
+    for x in 1..=10 {
+    if x % 3 != 0 {
+  println!("Number: {}", x);
+  } else {
+      continue
+  }
+  }
+  
+  
+  
 }
