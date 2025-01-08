b19097d26a756c445410a427e538790cd75b6168
Oksana Shyshkina <38759037-shyshkinaksysha@users.noreply.replit.com>
2025-01-08 15:50:55 +0000

    The final version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
 fn main() {
-    println!("Hello, world!");
+
+  //Basic Operations with values - mathematics
+  let a = 10;
+  let b = 3;
+
+  println!("Addition: {}", a + b);        
+  println!("Subtraction: {}", a - b);     
+  println!("Multiplication: {}", a * b); 
+  println!("Division: {}", a / b);       
+  println!("Remainder: {}", a % b); 
+  
+  //Basic Operations with values - comparing
+  let x = 5;
+  let y = 10;
+
+  println!("x == y: {}", x == y); 
+  println!("x != y: {}", x != y); 
+  println!("x > y: {}", x > y);   
+  println!("x < y: {}", x < y);   
+  println!("x >= y: {}", x >= y); 
+  println!("x <= y: {}", x <= y); 
+
+  //create new values and operations on it
+  let num1 = 20;
+  let num2 = 30;
+  println!("Addition: {}", num1 + num2);
+  println!("Subtraction: {}", num1 - num2);
+  println!("Multiplication: {}", num1 * num2);
+  println!("Division: {}", num1 / num2);
+  println!("Remainder: {}", num1 % num2);
+  println!("Is num1 == num2? {}", num1 == num2);
+  println!("Is num1 > num2? {}", num1 > num2);
+  println!("Is num2 <= num1? {}", num2 <= num1);
 }
