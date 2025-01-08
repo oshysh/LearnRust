06a45057fc8febdd8edc375378b0ef2e0ce8411d
Oksana Shyshkina <38759037-shyshkinaksysha@users.noreply.replit.com>
2025-01-08 16:17:49 +0000

    Half of lesson

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
 fn main() {
-    println!("Hello, world!");
+  // IF construction
+  let number = 10;
+
+  if number > 5 {
+      println!("The number is greater than 5.");
+  }
+
+  //IF-ELSE construction
+  let number = 10;
+
+  if number > 15 {
+      println!("The number is greater than 15.");
+  } else if number == 10 {
+      println!("The number is exactly 10.");
+  } else {
+      println!("The number is less than 10.");
+  }
+  //Don't understand
+  let condition = true;
+  let number = if condition { 5 } else { 10 }; 
+
+  println!("The value of number is: {}", number);
+  
+  // comparing temperature
+  let temperature  = 25;
+  
+  if temperature >=30 {
+    println!("It's hot outside!");
+    } else 
+    if temperature >= 15 {
+      println!("The weather is nice.");
+    } else {
+      println!("It's cold outside!");
+      }
+  
+       
+  
 }
